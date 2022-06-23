e7d6b5f0b0add3b26e751fdaa5cca1ae65000b5b
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2022-06-23 12:30:13 +0200

    added link on github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ export default function App() {
 	return (
 		<div className='App'>
 			<Weather />
+			<p className='text-center mt-2 text-muted code-link'>
+				<a
+					href='https://github.com/lisakuchmenko/react-weather-app'
+					target='_blank'
+					rel='noreferrer'
+					title='project on GitHub'
+				>
+					Open-source code
+				</a>{' '}
+				by Liza Kuchmenko
+			</p>
 		</div>
 	);
 }
